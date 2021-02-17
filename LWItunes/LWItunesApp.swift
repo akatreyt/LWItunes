3755d41033c48e84eae6d83c8a7c989af59fd864
TreyT <trey@88oak.com>
2021-02-16 20:15:05 -0600

    Initial Commit

---
+++ LWItunes/LWItunesApp.swift
@@ -0,0 +1,20 @@
+//
+//  LWItunesApp.swift
+//  LWItunes
+//
+//  Created by Gary Tartt on 2/16/21.
+//
+
+import SwiftUI
+
+@main
+struct LWItunesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
