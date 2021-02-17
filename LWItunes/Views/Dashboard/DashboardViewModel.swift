44d6d1600c545baaafb0a69e4dd4e1e9a9504bb5
TreyT <trey@88oak.com>
2021-02-17 16:31:27 -0600

    add comments, mainly to DashboardViewModel

--- LWItunes/Views/Dashboard/DashboardViewModel.swift
+++ LWItunes/Views/Dashboard/DashboardViewModel.swift
@@ -12,11 +12,15 @@ typealias SortedMediaInfo = [String: [MediaResult]]
 final class DashboardViewModel<Network : Fetchable> : ObservableObject, DashboardViewModelProtocol{
     
     init() {
+        // initalize the favorite manager with a specific StorageType
+        // from Favorable.swift
         self.favoriteManager = FavoriteManager(withStorageType: .Plist)
         self.numberOfFavorites = self.favoriteManager.favorites.count
         
         updateFavorites()
 
+        // this notificaiton is used when the user taps to add / remove
+        // a favorite media result. It updates numberOfFavorites
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(updateFavorites),
                                                name: .UpdateFavorites,
@@ -31,34 +35,84 @@ final class DashboardViewModel<Network : Fetchable> : ObservableObject, Dashboar
     
     @Published public var searchTerm = ""
     
+    ///
+    /// This is a toggle that is set when the user taps
+    /// on show favorites on the DashboardView
+    ///
     @Published public private(set) var showOnlyFavorite = false
 
+    ///
+    /// This is allows DashboardView to know when there is a network
+    /// request happening
+    ///
     @Published public private(set) var networkIsFetching : Bool = false
     
+    ///
+    /// This is used to store sorted data from
+    /// apiReturn.results
+    ///
+    /// The keys are the results kind [String], and the value is an array
+    /// of all items of the kind [MediaResult]
+    ///
     @Published public private(set) var sortedData = SortedMediaInfo()
     
+    ///
+    /// This error is passed through the networkManger
+    /// to show on the DashboardView if there is an error
+    ///
     @Published public private(set) var error: Error?
     
-    private var network = Network()
+    ///
+    /// This is allows for fetching of data. It is set in
+    /// LWItunesApp.swift
+    ///
+    /// DashboardView<Network>()
+    ///
+    private var netorkManager = Network()
 
+    ///
+    /// This is all the keys from the sortedData. This is used
+    /// on DashboardView to pouplate the action sheet filter
+    ///
     public private(set) var mediaKeys = [String]()
 
+    ///
+    /// This is used to to show the number of favorites on the
+    /// home screen. (Currently not is use)
+    ///
     public private(set) var numberOfFavorites : Int
     
+    ///
+    /// This is used to give access to the user's favorites
+    ///
     public private(set) var favoriteManager : Favorable
     
+    ///
+    /// This is the variable stores the results from a network request
+    /// It'll will be nil first run, and is used in DashboardView to
+    /// know when to show the search screen.
+    ///
     public private(set) var apiReturn : APIReturn?{
         didSet{
             resetSortedData()
         }
     }
     
+    ///
+    /// This is the key that is used on the main screen to reset the
+    /// filter back to all results in sortedData. Both the DashboardView and
+    /// DashboardViewModel use this to as a specifier
+    ///
     public let filterResetKey = "Reset"
     
+    ///
+    /// This function initiates a search based on the passed in
+    /// <Network : Fetchable> init
+    ///
     public func search(){
         do{
             networkIsFetching = true
-            try network.fetchFrom(endpoint: EndPoint.search,
+            try netorkManager.fetchFrom(endpoint: EndPoint.search,
                                   forTerm: searchTerm,
                                   completionHandler: { [weak self] result in
                                     DispatchQueue.main.async {
@@ -77,6 +131,15 @@ final class DashboardViewModel<Network : Fetchable> : ObservableObject, Dashboar
         }
     }
     
+    /// This function filters the current SortedMediaInfo,
+    /// thats stored in sortedData, to show items that belong
+    /// to a specific key
+    ///
+    /// Usage:
+    ///
+    /// - Parameter key : String: The kind the user wants to see
+    ///
+    /// - Returns: void
     public func filterMedia(forKey key : String){
         resetSortedData()
         if key != filterResetKey{
@@ -86,6 +149,10 @@ final class DashboardViewModel<Network : Fetchable> : ObservableObject, Dashboar
         }
     }
     
+    ///
+    /// This function is the public interfacing bool to view
+    /// only favorites on the main screen
+    ///
     public func toggleOnlyShowFavorites() {
         showOnlyFavorite = !showOnlyFavorite
         if showOnlyFavorite{
@@ -95,7 +162,14 @@ final class DashboardViewModel<Network : Fetchable> : ObservableObject, Dashboar
         }
     }
     
-    private func sort(data : [MediaResult]) -> [String : [MediaResult]]{
+    /// This function returns a SortedMediaInfo from a given data set
+    ///
+    /// Usage:
+    ///
+    /// - Parameter [MediaResult]: The media results to be sorted
+    ///
+    /// - Returns: SortedMediaInfo
+    private func sort(data : [MediaResult]) -> SortedMediaInfo{
         var tmpSortedData =  [String : [MediaResult]]()
         
         for result in data{
@@ -106,6 +180,9 @@ final class DashboardViewModel<Network : Fetchable> : ObservableObject, Dashboar
         return tmpSortedData
     }
     
+    ///
+    /// This function sort fetched data into SortedMediaInfo
+    ///
     private func resetSortedData(){
         if let _apiReturn = apiReturn{
             sortedData = sort(data: _apiReturn.results)
@@ -113,11 +190,18 @@ final class DashboardViewModel<Network : Fetchable> : ObservableObject, Dashboar
         }
     }
     
+    ///
+    /// This function sort favorites into SortedMediaInfo
+    ///
     private func sortFavoriteData(){
         sortedData = sort(data: self.favoriteManager.favorites)
         mediaKeys = sortedData.keys.sorted(by: <)
     }
     
+    ///
+    /// This function updates the public number of favorites and will
+    /// re-initalize the data set if we're currently viewing the favorites
+    ///
     @objc private func updateFavorites(){
         self.numberOfFavorites = favoriteManager.favorites.count
         
